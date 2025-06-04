5abdaa648fbdf9268541bc1152b6bdd55bac3327
lakshya <lakshyaparik99@gmail.com>
2025-06-04 18:00:21 +0530

    component understand

---
+++ REACT-BY-STUDYTONIGH/day-2/components/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Student from './Student'
+function App() {
+  return (
+    <>
+      <Student name="lakshya" age={20} />
+      <Student name="sahil" age={21} />
+      <Student name="sahil" age={21} />
+    </>
+  )
+}
+
+export default App
---
+++ REACT-BY-STUDYTONIGH/day-2/components/src/Student.jsx
@@ -0,0 +1,12 @@
+const Student= (props)=>{
+  return (
+    <div>
+      <h1>name: {props.name}</h1>
+      <h1>age: {props.age}</h1>
+      <hr />
+    </div>
+
+  )
+}
+
+export default Student
